359a60a664052b21d84e86866bb2f07553c853a2
SiriN2011 <siri02282011@gmail.com>
2025-07-21 12:19:07 -0400

    Pushing again

--- python_1/Ping Pong Game.py
+++ python_1/Ping Pong Game.py
@@ -5,9 +5,9 @@ import time
 # Screen Setup
 # ---------------------------
 win = turtle.Screen()
-win.title("Ping Pong Game by YOU")
-win.bgcolor("black")
-win.setup(width=800, height=600)
+win.title("Ping Pong Game by YOU") #screen title
+win.bgcolor("black") #screen background color
+win.setup(width=800, height=600) #screen background size
 
 # ---------------------------
 # Score Variables
@@ -19,23 +19,23 @@ score_b = 0
 # Paddle A
 # ---------------------------
 paddle_a = turtle.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("red")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.speed(0) #speed of paddle A
+paddle_a.shape("square") #shape of the backgroud
+paddle_a.color("red") #color of the background
+paddle_a.shapesize(stretch_wid=5, stretch_len=1) #size of the background
 paddle_a.penup()
-paddle_a.goto(-350, 0)
+paddle_a.goto(-350, 0) #coordinates of paddle A on the screen
 
 # ---------------------------
 # Paddle B
 # ---------------------------
 paddle_b = turtle.Turtle()
-paddle_b.speed(0)
-paddle_b.shape("square")
-paddle_b.color("red")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.speed(0) #speed of paddle B
+paddle_b.shape("square") #shape of the background
+paddle_b.color("red") #color of the background
+paddle_b.shapesize(stretch_wid=5, stretch_len=1) #size of the background
 paddle_b.penup()
-paddle_b.goto(350, 0)
+paddle_b.goto(350, 0) #coordinates of the background
 
 # ---------------------------
 # Ball
@@ -50,7 +50,7 @@ ball.dx = 7.00 * 1.05
 ball.dy = 7.00 * 1.05
 
 # ---------------------------
-# Score Display
+# This displayes how the score should be displayed on the screen
 # ---------------------------
 score_display = turtle.Turtle()
 score_display.speed(0)
@@ -61,7 +61,7 @@ score_display.goto(0, 260)
 score_display.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 # ---------------------------
-# Winner Display
+# This displays how the "winner" should be displayed on the screen
 # ---------------------------
 winner_display = turtle.Turtle()
 winner_display.speed(0)
@@ -71,7 +71,7 @@ winner_display.hideturtle()
 winner_display.goto(0, 0)
 
 # ---------------------------
-# Paddle Movement Functions
+# Paddle Movement Functions on the screen
 # ---------------------------
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -82,25 +82,28 @@ def paddle_a_down():
     y = paddle_a.ycor()
     if y > -240:
         paddle_a.sety(y - 20)
+        #Checks the paddle's current vertical location and moves it up and down 20 pixels when you clcik a button
 
 def paddle_b_up():
     y = paddle_b.ycor()
     if y < 250:
         paddle_b.sety(y + 20)
+        #Moves paddle B up 20 pixels
 
 def paddle_b_down():
     y = paddle_b.ycor()
     if y > -240:
         paddle_b.sety(y - 20)
+        #Moves paddle B down 20 units and makes sure it doesn't move off the screen
 
 # ---------------------------
-# Keyboard Bindings
+# Keys to move paddle a and b up, down, left, and right
 # ---------------------------
 win.listen()
-win.onkeypress(paddle_a_up, "w")
-win.onkeypress(paddle_a_down, "s")
-win.onkeypress(paddle_b_up, "Up")
-win.onkeypress(paddle_b_down, "Down")
+win.onkeypress(paddle_a_up, "w") #Moves Paddle A up when "w" key is pressed
+win.onkeypress(paddle_a_down, "s") #Moves Paddle A down when "s" key is pressed
+win.onkeypress(paddle_b_up, "Up") #Moves Paddle B up when "Up" key is pressed
+win.onkeypress(paddle_b_down, "Down") #Moves Paddle B up when "Down" key is pressed
 
 # ---------------------------
 # Function to Update Score Display
@@ -109,10 +112,10 @@ def update_score():
     score_display.clear()
     score_display.write(f"Player A: {score_a}  Player B: {score_b}",
                         align="center", font=("Courier", 24, "normal"))
-
+# Aligns the score in the center of the screen horizontally
 # ---------------------------
 # Game Loop
-# ---------------------------
+# Bouncs the ball off the wal and keeps track of the score
 while True:
     win.update()
 
